e32c3b7edf12dc958de99662c15f6d381af94813
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-28 01:53:00 +0100

    GraphViewer with no bugs

---
+++ Project/src/connection.h
@@ -0,0 +1,42 @@
+﻿#ifndef _CONNECTION_
+#define _CONNECTION_
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#ifdef linux
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#else
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+class Connection {
+ public:
+  Connection(short port);
+
+  bool sendMsg(string msg);
+  string readLine();
+ private: 
+#ifdef linux
+  int sock;
+#else
+  SOCKET sock;
+#endif
+};
+
+#endif

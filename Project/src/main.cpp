83b10359ede9f99c7181d8a40916ec73d9379bee
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-05-28 23:05:43 +0100

    Dumb line escaped...

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 int main() {
     cout << "Starting Waste Collect program" << endl;
-    cout << "Press ENTER" << endl;
     system("pause");
 
     FullGraph graph;
-    graph.preProcessGraph();
-    graph.graph.floydWarshallShortestPath();//get the shortest path--takes 5 min to process--
+    graph.preProcessGraph();                        //Pre Process the graph
+    graph.graph.floydWarshallShortestPath();        //get the shortest path--takes 5 min to process--
     graph.testCases();
 
     GraphViewer *gv = new GraphViewer(900, 900, false);

738d8dc821548ce0c1958e2b4251d142e987dd86
Andreia <andiigouveia@hotmail.com>
2019-05-28 23:21:52 +0100

    Merged

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -12,11 +12,11 @@ int main() {
     graph.graph.floydWarshallShortestPath();        //get the shortest path--takes 5 min to process--
     graph.testCases();
 
-    GraphViewer *gv = new GraphViewer(900, 900, false);
+   /* GraphViewer *gv = new GraphViewer(900, 900, false);
     gv->createWindow(900, 900);
 
     graph.showGraphViewer(*gv);
-    graph.paintNodes(*gv);
+    graph.paintNodes(*gv);*/
     system("pause");
     return 0;
 }
